9f479bc5ebda833ae660f2c53c63c481c7e83a4a
¨gabriel¨ <gabriel.he2010@hotmail.com>
2022-04-12 02:45:53 -0300

    adiciona exercicios do dia 1

---
+++ fundamentos/bloco-4-introducao-a-javaScript/dia-1-introducao/exercicio.js
@@ -0,0 +1,8 @@
+const a = 12;
+const b = 60;
+
+let adicao = console.log (a + b);
+let subtracao  = console.log (a - b);
+let multiplicacao = console.log (a * b);
+let divisao = console.log (a / b);
+let modulo = console.log (a % b);
---
+++ fundamentos/bloco-4-introducao-a-javaScript/dia-1-introducao/exercicios-3.js
@@ -0,0 +1,11 @@
+const numero1 = 12
+const numero2 = 66
+const numero3= 100
+
+if (numero1 > numero2 && numero1 > numero3){
+    console.log(numero1)
+} else if (numero2 > numero1 && numero2 > numero3){
+    console.log(numero2)
+} else {
+    console.log(numero3)
+}
